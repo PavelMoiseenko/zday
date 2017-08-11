e98d674dbd4845f4f8e6a8b1a1c11c16e22d23a6
Moiseenko <moiseenko@zfort.com>
2017-08-11 16:10:10 +0300

    realized ACF fields in header

--- wp-content/themes/base/front-page.php
+++ wp-content/themes/base/front-page.php
@@ -16,26 +16,26 @@
 
 
             // Get the API client and construct the service object.
-            $client = getClient();
-            $service = new Google_Service_Sheets($client);
-
-            //// Prints the names and majors of students in a sample spreadsheet:
-            //// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-            $spreadsheetId = /*'1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'*/'135RS03FkjkQsyJHV7AxWX6j1of2tcCO9036Vyp093rU';
-            $range = 'A2:B2';
-                        $response = $service->spreadsheets_values->get($spreadsheetId, $range);
-                        $values = $response->getValues();
-
-                        if (count($values) == 0) {
-                            print "No data found.\n";
-                        } else {
-                            var_dump($values);
-                            /*foreach ($values as $row) {
-                                // Print columns A and E, which correspond to indices 0 and 4.
-                                printf("%s, %s\n", $row[0], $row[4]);
-                            }*/
-                        }
-
+//            $client = getClient();
+//            $service = new Google_Service_Sheets($client);
+//
+//            //// Prints the names and majors of students in a sample spreadsheet:
+//            //// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+//            $spreadsheetId = /*'1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'*/'135RS03FkjkQsyJHV7AxWX6j1of2tcCO9036Vyp093rU';
+//            $range = 'A2:B2';
+//                        $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+//                        $values = $response->getValues();
+//
+//                        if (count($values) == 0) {
+//                            print "No data found.\n";
+//                        } else {
+//                            var_dump($values);
+//                            /*foreach ($values as $row) {
+//                                // Print columns A and E, which correspond to indices 0 and 4.
+//                                printf("%s, %s\n", $row[0], $row[4]);
+//                            }*/
+//                        }
+//
 
 //            $optParams = [];
 //            $optParams['valueInputOption'] = 'ROWS';
@@ -67,6 +67,8 @@
 
 
            // $response = $service->spreadsheets_values->update($spreadsheetId, $range, $requestBody, $optParams);
+
+
 
 
             $args = array(
